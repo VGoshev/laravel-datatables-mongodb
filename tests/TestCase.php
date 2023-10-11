bd3746acedef781112849be9ec3d5b7162d05f19
Vladimir Goshev <vgoshev@yandex-team.ru>
2023-10-11 17:50:12 +0300

    Fix tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -68,7 +68,7 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
     protected function getPackageProviders($app)
     {
         return [
-            \MongoDB\Laravel\MongodbServiceProvider::class,
+            \MongoDB\Laravel\MongoDBServiceProvider::class,
             \Yajra\DataTables\DataTablesServiceProvider::class,
         ];
     }
